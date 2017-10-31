64b19a88e0d547328db3f59ccada61650136f38e
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-31 17:44:32 -0600

    feature test for sessions, visitor can create an account, needs additional work

--- spec/features/sessions/a_visitor_can_create_an_account_spec.rb
+++ spec/features/sessions/a_visitor_can_create_an_account_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-describe "A visitor can go to the home page, click on a create Account, and" do
+describe "A visitor can go to the home page and it" do
   it "will load up a new page with form fields and will ask him to create An Account" do
 
     visit root_path
 
-    expect(page.status_code).to (200)
-    click_on("Create Account")
-    redirect_to signup_path
-    fill_in 'user[username]', with: "CheckOutMahKhakisBabySquirrel"
-    fill_in 'user[password]', with: "DiamondStuddedChinos"
+    expect(page.status_code).to eq(200)
 
-    click_on("Create My Account")
-    redirect_to users_path 
+    fill_in 'session[username]', with: "CheckOutMahKhakisBabySquirrel"
+    fill_in 'session[password]', with: "DiamondStuddedChinos"
+
+    click_on("Create Account")
+
+    expect(current_path).to eq(users_path)
 
     expect(page.status_code).to eq(200)
 
