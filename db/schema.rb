e3160b280d4215645458e8baeb8faf9f8866668f
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-31 12:43:18 -0600

    Migrate Categories table, added custom routes, build out model tests for items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030224749) do
+ActiveRecord::Schema.define(version: 20171031183254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "items", force: :cascade do |t|
     t.text "title"
