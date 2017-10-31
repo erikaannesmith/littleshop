c39f9dc146ed4b9aa465529a5155586721d22ac1
Tyjoo26 <Tyjoo21@gmail.com>
2017-10-31 17:44:03 -0600

    Modified routes to move down vanity url to bottom of routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items
-  get '/:name' => 'categories#show', as: :seecategory
+
   namespace :admin do
     resources :items, only: [:index, :new, :create]
   end
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  get '/:name' => 'categories#show', as: :seecategory
 end
