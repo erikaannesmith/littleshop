58406f3e1a31d12386ec44688604902481a0d29f
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-01 17:03:40 -0600

    Modified routes file by cleaning and reorganizing

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items
+  resources :categories, :except => [:show]
+  get ':slug' => 'categories#show'
 
   namespace :admin do
     resources :items, only: [:index, :new, :create]
   end
 
+  resources :users, only: [:new, :create, :show]
+
   root 'welcome#index'
 
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
   get '/signup' => 'users#new'
-  post '/users' => 'users#create'
 
-  get '/:name' => 'categories#show', as: :seecategory
+
 end
