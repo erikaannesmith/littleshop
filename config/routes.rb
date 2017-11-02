6c581f80e99991464fddf3f3393ee55596c498d6
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-02 15:14:11 -0600

    added dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/cart', to: 'carts#show'
 
 
-  
+
 
   post '/cart_add', to: 'carts#add', as: :add_item
   post '/cart_sub', to: 'carts#subtract', as: :subtract_item
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
 
   get '/signup' => 'users#new'
+
+  get '/dashboard' => "users#show", as: "dashboard"
+
 
   resources :categories, :except => [:show]
   get ':slug' => 'categories#show'
