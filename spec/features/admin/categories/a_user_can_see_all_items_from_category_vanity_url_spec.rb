1836100b04930710ff6ab60b5ab0b1adfce3871d
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-02 11:03:20 -0600

    Deleted duplicate feature test that accidently got dragged into admin folder

--- spec/features/admin/categories/a_user_can_see_all_items_from_category_vanity_url_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'a visitor can see all items associated with a category' do
-  it 'by typing in a vanity url that includes category.name instead of category.id' do
-    visit '/khakis'
-
-    expect(page).to have_content("Item List")
-  end
-end
