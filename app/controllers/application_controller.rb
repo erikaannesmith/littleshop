0019a56bbae9e37b6396a0862f89b38d948933fd
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-02 14:38:13 -0600

    save for pull

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_cart
 
+  helper_method :current_user
+
   def set_cart
     @cart ||= Cart.new(session[:cart])
   end
@@ -10,4 +12,22 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError.new("Not Found")
   end
+
+private
+
+ def login(user)
+   session[:user_id] = user.id
+ end
+
+ def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+ end
+
+ def logged_in?
+   !current_user.nil?
+ end
+
+ def auth
+   redirect_to login_url, alert: 'You must login to access that page' unless logged_in?
+ end
 end
