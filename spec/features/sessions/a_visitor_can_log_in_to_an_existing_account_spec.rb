02f11f993a43435affbdbc529a5418ec5f01ddd1
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-02 12:05:15 -0600

    Added feature test for log out functionality on user show, test passes

--- spec/features/sessions/a_visitor_can_log_in_to_an_existing_account_spec.rb
+++ spec/features/sessions/a_visitor_can_log_in_to_an_existing_account_spec.rb
@@ -14,6 +14,26 @@ describe "A visitor can go to the home page, click on a log in, and" do
     expect(current_path).to eq("/users/#{user.id}")
 
     expect(page.status_code).to eq(200)
-    expect(page).to have_content("Welcome Back User")
+    expect(page).to have_content("Welcome User!")
+
+  end
+
+  it "will have a log out page that will redirect back to root index" do
+    user = User.create(username:"CheckOutMahKhakisBabySquirrel", password:"DiamondStuddedChinos" )
+
+    visit root_path
+
+    expect(page.status_code).to eq(200)
+    fill_in 'session[username]', with: "CheckOutMahKhakisBabySquirrel"
+    fill_in 'session[password]', with: "DiamondStuddedChinos"
+
+    click_on("Log In")
+    expect(current_path).to eq("/users/#{user.id}")
+
+    expect(page.status_code).to eq(200)
+    expect(page).to have_content("Welcome User!")
+
+    click_on("Log Out")
+    expect(current_path).to eq(root_path)
   end
 end
