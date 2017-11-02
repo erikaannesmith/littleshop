9ee20ad40e40bb8faf1451a28fbde1a4746e563e
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-02 15:15:54 -0600

    added private methods for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError.new("Not Found")
   end
+
+  private
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def auth
+    redirect_to login_url, alert: 'You must login to access that page' unless logged_in?
+  end
 end
