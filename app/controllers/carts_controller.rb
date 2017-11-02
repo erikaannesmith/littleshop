30379172b3e657df9ecc784b29c4ec6fe64f1cd8
Adrian Lara <adrianblara@gmail.com>
2017-11-01 19:16:56 -0600

    fix some indents

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,33 +1,33 @@
 class CartsController < ApplicationController
-    include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TextHelper
 
-    def create
-        item = Item.find(params[:item_id])
+  def create
+    item = Item.find(params[:item_id])
 
-        @cart.add_item(item.id)
-        session[:cart] = @cart.contents
+    @cart.add_item(item.id)
+    session[:cart] = @cart.contents
 
-        flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)} in your cart."
-        redirect_to items_path
-    end
+    flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)} in your cart."
+    redirect_to items_path
+  end
 
-    def show
-        @items = @cart.contents.map { |f| Item.find(f[0].to_i) }
-    end
+  def show
+    @items = @cart.contents.map { |f| Item.find(f[0].to_i) }
+  end
 
-    def remove
-        @cart.remove_item(params[:item_id])
-        item = Item.find(params[:item_id])
+  def remove
+    @cart.remove_item(params[:item_id])
+    item = Item.find(params[:item_id])
 
-        flash[:notice] = "Successfully removed #{view_context.link_to item.title, item_path(item)} from your cart."
+    flash[:notice] = "Successfully removed #{view_context.link_to item.title, item_path(item)} from your cart."
 
-        redirect_to cart_path(@cart)
-    end
+    redirect_to cart_path(@cart)
+  end
 
-    def add
-      @cart.add_item(params[:item_id])
+  def add
+    @cart.add_item(params[:item_id])
 
-      redirect_to cart_path(@cart)
-    end
+    redirect_to cart_path(@cart)
+  end
 
 end
