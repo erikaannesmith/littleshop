ed81377b5d2153fffd5c0fb9e3864bab4f7d2e7a
Tyjoo26 <Tyjoo21@gmail.com>
2017-11-04 17:45:32 -0600

    added additional feature tests to fully pass user story 18

--- spec/features/admin/admin_sees_dashboard_spec.rb
+++ spec/features/admin/admin_sees_dashboard_spec.rb
@@ -11,4 +11,19 @@ describe 'when an admin logs in and visits admin dashboard' do
       expect(page).to have_content("Admin Dashboard")
 
     end
+
+    scenario 'when a registered user tries to access admin dashboard they get a 404' do
+      user = User.create(username: "tyJ", password: "123", role: 0)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_dashboard_path
+
+      expect(page).to have_content("The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved. If you are the application owner check the logs for more information.")
+    end
+
+    scenario 'when a visitor tries to access admin dashboard they get a 404' do
+      visit admin_dashboard_path
+
+      expect(page).to have_content("The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved. If you are the application owner check the logs for more information.")
+    end
 end
