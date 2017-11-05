e2245841b384735502edf6fa5c34bd4249d6d822
Adrian Lara <adrianblara@gmail.com>
2017-11-05 11:23:27 -0700

    cart persists upon login or account creation test written

---
+++ spec/features/carts/cart_persists_upon_account_creation_and_login_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "When a visitor adds an item to the cart" do
+  before :each do
+      @category = create(:category)
+      @item = create(:item, category: @category)
+      @user = User.create(username: "test", password: "test")
+
+      visit items_path
+
+      click_link "Add to Cart"
+      
+      expect(page).to have_content("Cart: 1")
+  end
+
+  describe "And they log in" do
+    it "the cart persists" do
+      fill_in "session[username]", with: "test"
+      fill_in "session[password]", with: "test"
+      click_button "Log In"
+
+      expect(page).to have_content("Cart: 1")
+    end
+  end
+
+  describe "And they create a new account" do
+    it "the cart persists" do
+      click_link "Create Account"
+      fill_in "user[username]", with: "test-1"
+      fill_in "user[password]", with: "test"
+
+      expect(page).to have_content("Cart: 1")
+    end
+  end
+end
