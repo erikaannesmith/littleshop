dfff2bb2a062691bdbe921790a8b003bb363b5c7
Adrian Lara <adrianblara@gmail.com>
2017-11-05 14:45:06 -0700

    test set up for admin to see all orders in admin dashboard

--- spec/features/admin/admin_logs_in_spec.rb
+++ spec/features/admin/admin_logs_in_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 describe 'when an admin logs in' do
-  it 'is redirected to admin dashboard' do
-    admin = User.create(username: '123', password: "123", role: 1)
+  before(:each) do
+    @category = create(:category)
+    @users = create_list(:user, 2)
+    @items = create_list(:item, 5, category: @category)
+    Order.create(total_price: (@items[0].price * 2), user_id: @users[0].id)
+    Order.create(total_price: (@items[1].price * 3), user_id: @users[0].id)
+    Order.create(total_price: (@items[2].price * 2), user_id: @users[0].id)
+    Order.create(total_price: (@items[3].price * 2), user_id: @users[1].id)
+    Order.create(total_price: (@items[4].price * 2), user_id: @users[1].id)
+    @admin = User.create(username: '123', password: "123", role: 1)
 
     visit root_path
 
@@ -10,8 +18,17 @@ describe 'when an admin logs in' do
     fill_in 'session[password]', with: "123"
 
     click_on("Log In")
+  end
 
+  it 'is redirected to admin dashboard' do
     expect(current_path).to eq(admin_dashboard_path)
+  end
 
+ it "the admin sees a listing of all orders" do
+    expect(page).to have_link("Order ##{Order.first.id}")
+    expect(page).to have_link("Order ##{Order.second.id}")
+    expect(page).to have_link("Order ##{Order.third.id}")
+    expect(page).to have_link("Order ##{Order.fourth.id}")
+    expect(page).to have_link("Order ##{Order.fifth.id}")
   end
 end
