dc8cebb7e64ec3e5183b118228f676f5b9c3e73e
Erika Smith <erikaannesmith@gmail.com>
2017-11-07 13:43:02 -0700

    Admin can see a table of items and edit them

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,9 @@
 class Admin::ItemsController < Admin::BaseController
 
+  def index
+    @items = Item.all
+  end
+
   def new
     @item = Item.new
   end
@@ -13,10 +17,21 @@ class Admin::ItemsController < Admin::BaseController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to admin_items_path
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :price, :image, :category_id)
+    params.require(:item).permit(:title, :description, :price, :image, :status, :category_id)
   end
 
 end
---
+++ spec/features/admin/admin_can_view_table_of_all_items_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe "Admin visits admin items page" do
+  before :each do 
+    admin = User.create(username: '123', password: "123", role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  end
+  
+  it "they see a link for the items page on the dashboard" do
+    visit admin_dashboard_path
+
+    expect(page).to have_link("All Items")
+
+    click_on "All Items"
+
+    expect(current_path).to eq(admin_items_path)
+  end
+
+  it "admin can see all items in a table" do
+    category = create(:category)
+    item = create(:item, category: category)
+    
+    visit admin_dashboard_path
+    click_on "All Items"
+
+    expect(page).to have_content(item.title)
+    expect(page).to have_content(item.description)
+    expect(page).to have_content(item.status)
+    expect(page).to have_link("Edit")
+  end
+
+  it "admin can edit item from items table" do
+    category = create(:category)
+    item = create(:item, category: category)
+    
+    visit admin_dashboard_path
+    click_on "All Items"
+    click_on "Edit"
+
+    expect(current_path).to eq(edit_admin_item_path(item))
+
+    fill_in "item[title]", with: "12345"
+    save_and_open_page
+    click_on "Update Item"
+
+    expect(current_path).to eq(admin_items_path)
+    expect(page).to have_content("12345")
+  end
+end
