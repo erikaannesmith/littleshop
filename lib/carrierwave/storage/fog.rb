7598932a0f1d9082a3980f88b900fa76b43e6305
Adrian Lara <adrianblara@gmail.com>
2017-11-08 23:40:53 -0700

    delete unnecessary file

--- lib/carrierwave/storage/fog.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     'AKIAIECWIW36JAP5EB5Q',
-    aws_secret_access_key: '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',
-  }
-  config.fog_directory  = 'littleshopitemimages'
-end
